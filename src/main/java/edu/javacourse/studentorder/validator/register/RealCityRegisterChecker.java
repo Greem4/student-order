c5444c4b6e117ad027a03c28ecbc822c06b22b4d
Greem4 <i@greem4.ru>
2023-10-26 13:33:40 +0300

    R 108, update ReST

--- src/main/java/edu/javacourse/studentorder/validator/register/RealCityRegisterChecker.java
+++ src/main/java/edu/javacourse/studentorder/validator/register/RealCityRegisterChecker.java
@@ -1,9 +1,10 @@
 package edu.javacourse.studentorder.validator.register;
 
+import edu.javacourse.studentorder.config.Config;
 import edu.javacourse.studentorder.domain.register.CityRegisterRequest;
 import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.domain.Person;
-import edu.javacourse.studentorder.exception.TransportException;
+import edu.javacourse.studentorder.exception.CityRegisterException;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -12,16 +13,20 @@ import javax.ws.rs.core.MediaType;
 
 public class RealCityRegisterChecker implements CityRegisterChecker {
     public CityRegisterResponse checkPerson(Person person)
-            throws ClassCastException, TransportException {
+            throws CityRegisterException {
+        try {
+            CityRegisterRequest request = new CityRegisterRequest(person);
 
-        CityRegisterRequest request = new CityRegisterRequest(person);
+            Client client = ClientBuilder.newClient();
+            CityRegisterResponse response = client.target(
+                            Config.getProperty(Config.CR_URL))
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.entity(request, MediaType.APPLICATION_JSON))
+                    .readEntity(CityRegisterResponse.class);
+            return response;
+        }catch (Exception e) {
+            throw new CityRegisterException("1", e.getMessage() ,e);
+        }
 
-        Client client = ClientBuilder.newClient();
-        CityRegisterResponse response = client.target("http://localhost:8080/city-register-1.0/rest/check")
-                .request(MediaType.APPLICATION_JSON)
-                .post(Entity.entity(request, MediaType.APPLICATION_JSON))
-                .readEntity(CityRegisterResponse.class);
-
-        return response;
     }
 }
